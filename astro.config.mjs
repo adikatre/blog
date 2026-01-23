371517963c5abd114d0577912ec9d60d0c2e4f50
adikatre <adityad39c@gmail.com>
2026-01-22 21:45:20 -0800

    try to fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://adikatre.github.io/blog',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
