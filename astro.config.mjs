c01eccf115b8cfc32852cac574a35651e5010343
adikatre <adityad39c@gmail.com>
2026-01-22 21:39:12 -0800

    Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://adikatre.github.io/blog',
+	site: 'https://adikatre.github.io',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
